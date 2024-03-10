3aabf6259e6875e8984c9a72e26827551e91f7d8
Pongpol TS <134462783+ppts2545@users.noreply.github.com>
2024-03-10 15:04:07 +0700

    change to show dtabase querry in firstpage2

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from playground.models import Person
 
 def say_hello(request):
-    
-    return render(request, 'hello.html')
+    all_person = Person.objects.all()
+    return render(request, 'hello.html', {"all_person":all_person})
 
 def page1Render(request):
     """Renders the 'page1.html' template without any dynamic content."""
