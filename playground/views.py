52c032a47d0cd3df903f0a85b0610e1ca38a7dcd
Pongpol TS <134462783+ppts2545@users.noreply.github.com>
2024-03-10 22:16:56 +0700

    update

--- playground/views.py
+++ playground/views.py
@@ -7,18 +7,24 @@ def say_hello(request):
     return render(request, 'hello.html', {"all_person":all_person})
 
 def page1Render(request):
-    """Renders the 'page1.html' template without any dynamic content."""
     return render(request, 'page1.html')
 
+
 def pageFormRender(request):
-    """Renders the 'form.html' template without any dynamic content."""
+    
     if request.method == "POST":
         #รับข้อมูล
-        name = request.POST["name"]
-        age = request.POST["age"]
-        print(name ,age)
-        return redirect("/")
+        name = request.POST['name']
+        age = request.POST['age']
+        print(name , age)
         #บันทึกข้อมูล
+        person = Person.objects.create(
+            name = name,
+            age = age
+        )
+        person.save()
+        #เปลี่ยนเส้นทาง
+        return redirect("/")
      
     else :
         return render(request, 'form.html')
